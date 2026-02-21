4eb29193adc6722ae4d07a8f8890c55d2fc2f430
Thomas Brettin <brettin@anl.gov>
2026-02-21 17:41:00 +0000

    comment

--- examples/scatter_gather_coli.sh
+++ examples/scatter_gather_coli.sh
@@ -1,3 +1,7 @@
-python ../scripts/wait_for_vllm_servers.py --hostfile ../scripts/hostfile --output ../scripts/swarm.hostfile
+HOSTFILE=$1
 
-python ../examples/scatter_gather_coli.py /home/brettin/ModCon/brettin/Aurora-Inferencing/examples/TOM.COLI/batch_1/ --hostfile ../scripts/swarm.hostfile --num-files 16 --output test.openai.batch.txt --model openai/gpt-oss-120b --batch-size 2000
+python ../scripts/wait_for_vllm_servers.py --hostfile $HOSTFILE --output ../scripts/swarm.hostfile
+
+# batch size is 2000 prompts sent across assuming 4 servers,
+# then each vllm requests queue will get batches of 500 prompts.
+python ./scatter_gather_coli.py /home/brettin/ModCon/brettin/Aurora-Inferencing/examples/TOM.COLI/batch_1/ --hostfile ../scripts/swarm.hostfile --num-files 16 --output test.openai.batch.txt --model openai/gpt-oss-120b --batch-size 2000
